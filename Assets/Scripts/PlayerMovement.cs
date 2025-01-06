e4e1b54415fe44f9ea938711551f6a7db04b876d
RoboBaby570 <79467224+RoboBaby570@users.noreply.github.com>
2025-01-06 00:56:04 +0000

    Movement, camera, animation.
    Push includes
    New Movement Script
    Animation Setup for roopie including left and right flipping of sprites.
    removal script FlipMovement
    Moved Timer.cs to scripts folder (keep it tidy!)
    Camera Script was deleted to create a new one with a different name (unity hates script renaming)
    Camera movement implemented in a way that makes it more customisable in the future.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Oroopie’s movement speed
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public float moveSpeed; // Roopies movement speed
+    [HideInInspector]
+    public float lastHorizontalVector;// for tracking the last horizontal vector for the animator
+    [HideInInspector]
+    public float lastVerticalVector;// for tracking the last horizontal vector for the animator
+    Rigidbody2D rb;
+    [HideInInspector]
+    public Vector2 moveDir;
 
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
-        // Move Oroopie based on input
-        transform.Translate(new Vector3(moveX, moveY, 0));
+        InputManagement();
     }
+
+    void FixedUpdate()
+    {
+        Move(); //specifically in fixed update to make sure its not tied to fps and is consistent on all machines
+    }
+
+    void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        //legacy movement is easier to implement and functions fine in this instance
+        moveDir = new Vector2(moveX, moveY).normalized;
+        //tracks which way roopie is moving before only moving on the Y axis. keeps roopie facing a direction when moving up and down
+        if (moveDir.x != 0)
+        {
+            lastHorizontalVector = moveDir.x;
+        }
+        if (moveDir.y != 0)
+        {
+            lastVerticalVector = moveDir.y;
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+
+    
 }
